5d173606812e906db590db36262c58beba913f1e
Valentin Berger <Valentin.berger38@gmail.com>
2018-10-28 16:18:14 -0400

    Roboto font
    * Added the Roboto font and apply it to the 2 forms.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 
 #include <QDebug>
+#include <QFontDatabase>
 
 using namespace std;
 
@@ -11,6 +12,8 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("Aezy");
 	QCoreApplication::setApplicationName(qApp->applicationName());
 	QApplication a(argc, argv);
+
+	QFontDatabase::addApplicationFont(":/fonts/roboto/res/fonts/Roboto-Regular.ttf");
 
 	MainWindow w;
 	w.show();
